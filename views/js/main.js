de6514f072479defc38323b7c4eaae5800a023ce
Brian Buchalter <bal711@gmail.com>
2015-02-01 10:17:42 -0700

    prevent repeated DOM node querying

--- views/js/main.js
+++ views/js/main.js
@@ -450,10 +450,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var nodeList = document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < nodeList.length; i++) {
+      var dx = determineDx(nodeList[i], size);
+      var newwidth = (nodeList[i].offsetWidth + dx) + 'px';
+      nodeList[i].style.width = newwidth;
     }
   }
 
