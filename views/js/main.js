4c5a7e01fb7475e4949470c5844da0b1ff10b4bc
Brian Buchalter <bal711@gmail.com>
2015-01-31 21:51:28 -0700

    Update positions only after making all calculations

--- views/js/main.js
+++ views/js/main.js
@@ -503,9 +503,14 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var newPositions = [];
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    newPositions.push(items[i].basicLeft + 100 * phase + 'px');
+  }
+
+  for (var i = 0; i < items.length; i++) {
+    items[i].style.left = newPositions[i];
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
